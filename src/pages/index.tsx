075264979687d9f2908516330f805814dbd44d16
Ezequiel Magalhães <ezequiel@csfdigital.corp>
2023-05-03 10:55:24 -0300

    Deploy-errors-6

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,8 @@ import Footer from '@/src/components/footer/Footer'
 import Header from '@/src/components/header/Header'
 import Navbar from '@/src/components/navbar/Navbar'
 import NavbarMobile from '@/src/components/navbarMobile/NavbarMobile'
-import { IndexContainer } from './styles/styles'
-// import Main from '@/src/components/main/MainComponent'
+import { IndexContainer } from '@/styles/styles/styles'
+import Main from '@/src/components/mainComponent/MainComponent'
 
 export default function Home() {
   const [navBarOption, setNavBarOption] = useState(0);
@@ -19,7 +19,7 @@ export default function Home() {
         <Header />
         <Navbar navBarOptionNumber={navBarOption} handleNavBarOption={handleNavBarOption} />
         <NavbarMobile navBarOptionNumber={navBarOption} handleNavBarOption={handleNavBarOption} />
-        {/* <Main navBarOptionNumber={navBarOption} handleNavBarOption={handleNavBarOption} /> */}
+        <Main navBarOptionNumber={navBarOption} handleNavBarOption={handleNavBarOption} />
         <Footer />
       </IndexContainer>
       )
